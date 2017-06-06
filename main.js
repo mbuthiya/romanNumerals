71479d1cef267e37bd9a2cc2a381a413d3cd274a
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-06-06 15:09:37 +0300

    Create user Interface logic
    created the user interface logic and  connected the input form to get the
     value

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+// Business Logic
 
+
+
+// User interface logic
+$(document).ready(function () {
+
+  // form submit
+  $("#numInputForm").submit(function () {
+
+    var numInput=$("#inputForm").val();
+    $("#inputForm").val("");
+
+
+    event.preventDefault();
+  })
+
+})
